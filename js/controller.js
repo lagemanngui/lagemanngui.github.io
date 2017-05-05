cf19f70c94df810100e84e6a26c0c54f00f41b02
Guilherme Lagemann <g.unplugged@gmail.com>
2017-05-04 23:31:18 -0300

    Starting...

--- js/controller.js
+++ js/controller.js
@@ -2,3 +2,16 @@ $(document).ready(function()
 {
 
 });
+
+function createWindow(where, title)
+{
+  $(where).append(
+    '<div class="window__top"><div class="window__title"><h1>'+title+'</h1></div><div class="window__hide"></div></div>'
+  );
+}
+function setWindowText(where, text)
+{
+  $(where).append(
+    '<div class="window__text"><p>'+text+'</p></div>'
+  );
+}
